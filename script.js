cc760521c751e8505cfcaee39bdab96ad0cbc244
Kai <mordechaigerstley@gmail.com>
2021-10-18 16:31:27 -0400

    Made start button Work in the js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const startButton = document.getElementById("start-btn");
+const questionBoxElement = document.getElementById("question-box");
+startButton.addEventListener("click", startGame);
+function startGame() {
+  console.log("testing");
+  startButton.classList.add("hidden");
+  questionBoxElement.classList.remove("hidden");
+}
+function setNextQuestion() {}
 
+function selectAnswer() {}
